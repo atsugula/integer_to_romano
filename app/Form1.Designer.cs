f136369f83f0dc6ffaa1d2c522c9b63954099d06
ATSU <atsgula@gmail.com>
2023-03-21 00:16:41 -0500

    Agregar archivos de proyecto.

---
+++ app/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.txtNumber);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnConvert);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(233, 196);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnConvert.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(41, 80);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(145, 48);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "CONVERTIR";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese el número a convertir:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnClear.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(41, 134);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(145, 48);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "LIMPIAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(41, 42);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(145, 20);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 193);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Ejercicio en clase";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtNumber;
+    }
+}
+
---
+++ app/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace app
+{
+    public partial class Form1 : Form
+    {
+
+        int number;
+        String convertRoman;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNumber.Text = "0";
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validamos que no este vacio o nulo
+                if (String.IsNullOrEmpty(txtNumber.Text)) throw new Exception("No se aceptan campos vacíos.");
+                // Guardamos el número en una variable
+                number = int.Parse(txtNumber.Text);
+                // Validamos que no sea mayor o igual a 4000
+                if (number >= 4000) throw new Exception("No se acepta un número mayor o igual a 4000.");
+                else if (number <= 0) throw (new Exception("No se acepta números menores o iguales a 0."));
+                // Convertimos esta monda
+                String message = $"El resultado de convertir " +
+                    $"su número {txtNumber.Text} a números romanos es: ";
+                message += converToRoman(number);
+                MessageBox.Show(message, "INFORMACION", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // Convertimos el número a numeros romanos
+        public string converToRoman(int num)
+        {
+            /****************************************
+             * Declaramos las variables necesarias *
+            ****************************************/
+            int units, dec, cent, uMil, temp;
+            String result = "";
+            /****************************
+             * Separamos las U. de mil *
+             * Centens y Decenas       *
+            ****************************/
+            uMil = num / 1000;
+            cent = num / 100;
+            dec = num / 10;
+            /***************************
+             * Separamos las unidades *
+            ***************************/
+            temp = num % 1000; // Por si tiene en unidades
+            temp = temp % 100; // Por si tiene en decenas
+            units = temp % 10;
+            /* Hacemos las validaciones por U. mil */
+            switch (uMil) {
+                case 1:
+                    result = "M";
+                    break;
+                case 2:
+                    result = "MMM";
+                    break;
+                case 3:
+                    result = "MMM";
+                    break;
+            }
+            /* Hacemos las validaciones por Centenas */
+            switch (cent) {
+                case 1:
+                    result += "C";
+                    break;
+                case 2:
+                    result += "CC";
+                    break;
+                case 3:
+                    result += "CC";
+                    break;
+                case 4:
+                    result += "CD";
+                    break;
+                case 5:
+                    result += "D";
+                    break;
+                case 6:
+                    result += "DC";
+                    break;
+                case 7:
+                    result += "DCC";
+                    break;
+                case 8:
+                    result += "DCCC";
+                    break;  
+                case 9:
+                    result += "CM";
+                    break;
+            }
+            /* Hacemos las validaciones por Decenas */
+            switch (dec) {
+                case 1:
+                    result += "X";
+                    break;
+                case 2:
+                    result += "XX";
+                    break;
+                case 3:
+                    result += "XXX";
+                    break;
+                case 4:
+                    result += "XL";
+                    break;
+                case 5:
+                    result += "L";
+                    break;
+                case 6:
+                    result += "LX";
+                    break;
+                case 7:
+                    result += "LXX";
+                    break;
+                case 8:
+                    result += "LXXX";
+                    break;
+                case 9:
+                    result += "XC";
+                    break;
+            }
+            /* Hacemos las validaciones por Unidades */
+            switch (units)
+            {
+                case 1:
+                    result += "I";
+                    break;
+                case 2:
+                    result += "II";
+                    break;
+                case 3:
+                    result += "III";
+                    break;
+                case 4:
+                    result += "IV";
+                    break;
+                case 5:
+                    result += "V";
+                    break;
+                case 6:
+                    result += "VI";
+                    break;
+                case 7:
+                    result += "VII";
+                    break;
+                case 8:
+                    result += "VIII";
+                    break;
+                case 9:
+                    result += "IX";
+                    break;
+            }
+            return result;
+        }
+        // Volver a ceros
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNumber.Text = "0";
+        }
+    }
+}
